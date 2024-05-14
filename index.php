3fe5f6f55ad91e5d92cd806cf3a82324f432ea60
Mateus <mateusrochamr133@gmail.com>
2024-05-14 13:58:49 -0300

    Aula 01 de php com o professor Eduardo do Senac Santos, exercicios com php dentro de um formulario

--- index.php
+++ index.php
@@ -1,42 +1,110 @@
 <!doctype html>
-<html lang="en">
-
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<html lang="pt-br">
+ 
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap demo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-
+ 
 <body>
-    
-    <div class="container">
-        <h1>Formulário</h1>
+  <div class="container">
 
-        <hr>
+    <h1>Formulário</h1>
+
+    <form action="script.php" method="POST">
+
+      <div class="row justify-content-center mt-5">
+
+        <div class="col-md3">
+
+          <div class="mb-3">
+
+            <label for="inputNome" class="form-label">Valor 01</label>
+            <input type="text" class="form-control" name="valor01" placeholder="digite um valor">
+
+          </div>
+
+        </div>
+
+      </div>
+
+      <div class="row justify-content-center mt-5">
+
+        <div class="col-md3">
+
+          <div class="mb-3">
+            <label for="inputNome" class="form-label">Valor 02</label>
+            <input type="text" class="form-control" name="valor02" placeholder="digite um valor">
+          </div>
+
+        </div>
+
+      </div>
+
+      <button type="submit" class="btn btn-primary">Verificar</button>
+
+    </form>
+
+  </div>
+ 
+  <div class="container-Temp">
+
+    <h1>Temperatura</h1>
+
+    <form action="script.php" method="POST">
+
+      <div class="row justify-content-center mt-5">
+
+        <div class="col-md3">
+
+          <div class="mb-3">
+
+            <label for="inputNome" class="form-label">Temperatura</label>
+            <input type="text" class="form-control" name="temperatura" placeholder="digite um valor">
+
+          </div>
+
+        </div>
+
+      </div>
+
+      <button type="submit" class="btn btn-primary">Verificar</button>
+
+    </form>
+
+  </div>
+ 
+    <div class="container-oleo">
+
+        <h2>Volume Oleo</h2>
+
         <form action="script.php" method="POST">
 
-            <div class="row jutify-content-center mt-5">
+            <div class="row justify-content-center mt-5">
 
                 <div class="col-md3">
 
                     <div class="mb-3">
-                        <label for="inputNome" class="form-label">Valor 01</label>
-                        <input type="text" class="form-control" name="valor1" placeholder="Digite um Valor">
+
+                        <label for="inputNome" class="form-label">Volume</label>
+                        <input type="text" class="form-control" name="valor_volume" placeholder="digite um valor">
+
                     </div>
 
                 </div>
 
             </div>
-            
 
-            <div class="row jutify-content-center mt-5">
+            <div class="row justify-content-center mt-5">
 
                 <div class="col-md3">
 
                     <div class="mb-3">
-                        <label for="inputNome" class="form-label">Valor 02</label>
-                        <input type="text" class="form-control" name="valor2" placeholder="Digite um Valor">
+                        <label for="inputNome" class="form-label">altura</label>
+                        <input type="text" class="form-control" name="altura" placeholder="digite um valor">
                     </div>
 
                 </div>
@@ -44,18 +112,115 @@
             </div>
 
             <button type="submit" class="btn btn-primary">Verificar</button>
-        
+
         </form>
 
     </div>
+ 
+    <div class="container-viagem">
 
-    <!-- Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <h2>Viagem</h2>
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+        <form action="script.php" method="POST">
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>   
+            <div class="row justify-content-center mt-5">
+
+                <div class="col-md3">
+
+                    <div class="mb-3">
+                        <label for="inputNome" class="form-label">Tempo</label>
+                        <input type="text" class="form-control" name="tempo_viagem" placeholder="digite um valor">
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="row justify-content-center mt-5">
+
+                <div class="col-md3">
+
+                    <div class="mb-3">
+                        <label for="inputNome" class="form-label">velocidade</label>
+                        <input type="text" class="form-control" name="velocidade_media" placeholder="digite um valor">
+                    </div>
+
+                </div>
+
+            </div>
+     
+            <button type="submit" class="btn btn-primary">Verificar</button>
+
+        </form>
+
+    </div>
+ 
+ 
+    <div class="container-Venda">
+
+        <h1>Temperatura</h1>
+
+        <form action="script.php" method="POST">
+
+            <div class="row justify-content-center mt-5">
+
+                <div class="col-md3">
+
+                    <div class="mb-3">
+                        <label for="inputNome" class="form-label">Custo Produto</label>
+                        <input type="text" class="form-control" name="custo_produto" placeholder="digite um valor">
+                    </div>
+
+                </div>
+
+            </div>
+
+            <button type="submit" class="btn btn-primary">Verificar</button>
+
+        </form>
+
+    </div>
+ 
+ 
+    <div class="container-Prestacao">
+
+        <h1>Prestacao</h1>
+
+            <form action="script.php" method="POST">
+
+                <div class="row justify-content-center mt-5">
+
+                    <div class="col-md3">
+
+                        <div class="mb-3">
+
+                            <label for="inputNome" class="form-label">Prestacao</label>
+                            <input type="text" class="form-control" name="prestacao" placeholder="digite um valor">
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <button type="submit" class="btn btn-primary">Verificar</button>
+
+            </form>
+
+    </div>
+ 
+ 
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+ 
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+    crossorigin="anonymous"></script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+    integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+    crossorigin="anonymous"></script>
     
 </body>
-
+ 
 </html>
--- script.php
+++ script.php
@@ -1,30 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
+ 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
-    <body>
-    
-            <?php
-
-                $valor01 = $_POST['valor1'];
-
-                $valor02 = $_POST['valor2'];
-    
-                echo "O valor digitado em valor1 foi:"  .$valor01."<br>";
-                
-                echo "<br> O valor digitado em valor1 foi:"  .$valor02;
-
-                $resultado = $valor01 + $valor02;
-
-                echo "<br> A soma de" .$valor01. "e" .$valor02. "da:" .$resultado;
-            ?>
-
-    </body>
-
+ 
+<body>
+ 
+<?php
+/* Este aqui é o 1 Exercício que realizamos  */
+/*$valor01=$_POST['valor01'];
+$valor02=$_POST['valor02'];
+echo " O valor digitado em valor01 foi: ".$valor01."<br>";
+echo " O valor digitado em valor02 foi: ".$valor02;
+$resultado=$valor01+$valor02;
+echo"<br> A soma dos valores" .$valor01. "e" .$valor02. "são: " .$resultado;
+ 
+$temperatura=$_POST['temperatura'];
+$resultado=$temperatura*9/5+32;
+echo " <br> O valor digitado em temperatura é: ".$resultado;
+ 
+$valor01=$_POST['valor_volume'];
+$valor02=$_POST['altura'];
+$resultado=3.14159*($valor01*$valor01)*$valor02;
+echo"<br> o volume da lata de óleo é ".$resultado;
+ 
+ 
+ 
+$valor01=$_POST['tempo_viagem'];
+$valor02=$_POST['velocidade_media'];
+$distancia=$valor01*$valor02;
+$litrosUsados=$distancia/12;
+echo "<br> A distancia percorrida foi de: ".$distancia."km <br>e utilizou" .$litrosUsados. "litros";*/
+ 
+ 
+$custo=$_POST['custo_produto'];
+$resultado=$custo+(0.30*100);
+echo " <br> O valor da venda é: ".$resultado;
+ 
+ 
+$prestacao=$_POST['prestacao'];
+$resultado=$prestacao+($prestacao*(20/100))*2;
+echo " <br> O valor da prestação é: ".$resultado;
+ 
+ 
+ 
+?>
+ 
+</body>
+ 
 </html>
 
 
